b70ab6e1d23e5bf382bb5314dd323c1bf3183536
viniciusoliveiras <vinitag190@gmail.com>
2022-11-30 16:10:41 -0300

    fix build errors

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,40 @@ import { Data as AppsType } from "../pages/api/apps";
 import { FormApps } from "./formapps";
 import styles from "./page.module.css";
 
-const fetchApps = async () =>
-  fetch(`${process.env.NEXT_PUBLIC_VERCEL_URL}/api/apps`, {
-    cache: "no-store",
-  }).then((res) => res.json() as Promise<AppsType>);
+const fetchApps = async (): Promise<AppsType> => [
+  {
+    name: "Educação",
+    url: "https://api.vercel.com/v1/integrations/deploy/prj_y5FuDNzCY0OsTmI42c68Bgnbmxp6/HJiKZbPbeW",
+  },
+  {
+    name: "Contratos",
+    url: "https://api.vercel.com/v1/integrations/deploy/prj_HVhZOqRUIg3g7dFezPEyBPEOAxLQ/zHhbPFUzSZ",
+  },
+  {
+    name: "Empresarial",
+    url: "https://api.vercel.com/v1/integrations/deploy/prj_a2eWi1SAEfb1BlDnVw1I4BEbqcDv/0J1ZghFxeD",
+  },
+  {
+    name: "Indicadores",
+    url: "https://api.vercel.com/v1/integrations/deploy/prj_08FnUnF2HkICrfyNJ24Puh7hyp7m/IHucQJ01dN",
+  },
+  {
+    name: "Ordem de Serviço",
+    url: "https://api.vercel.com/v1/integrations/deploy/prj_UymkcGEbKZuTAw84PuyE9uJBThKp/LFUwlJNhs4",
+  },
+  {
+    name: "Orçamento",
+    url: "https://api.vercel.com/v1/integrations/deploy/prj_Y5eFEJz8nmOU624SUjO58FcuwRm9/oN8zNbu0uw",
+  },
+  {
+    name: "Apps",
+    url: "https://api.vercel.com/v1/integrations/deploy/prj_AJfTTnY20QvPSAUyj6YiYQMhydFJ/fabA2krX9A",
+  },
+  {
+    name: "CRM",
+    url: "https://api.vercel.com/v1/integrations/deploy/prj_whLEApJFXdMAKfe4yLqMsPJt4agF/AGsCU7WAx1",
+  },
+];
 
 export default async function Home() {
   const apps = await fetchApps();
