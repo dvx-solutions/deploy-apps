bafe989be4492f6ebae945ac57d29f4da5fa454c
Vinícius Oliveira <vinitag190@gmail.com>
2023-11-27 15:59:05 -0300

    config CORS

--- src/app/api/portal-apps/route.ts
+++ src/app/api/portal-apps/route.ts
@@ -5,5 +5,12 @@ export const dynamic = "force-dynamic";
 export async function GET() {
   const data = await getPortalApps();
 
-  return Response.json(data);
+  return new Response(JSON.stringify(data), {
+    status: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization",
+    },
+  });
 }
